999dc226eb987b9e1d33fb70d6243ac9381aeaac
Andrei <An.S-1@yandex.ru>
2025-07-15 00:43:52 +0300

    add Task

--- handbook/src/main/java/ru/practicum/Task3.java
+++ handbook/src/main/java/ru/practicum/Task3.java
@@ -2,25 +2,25 @@ package ru.practicum;
 
 import java.io.*;
 
-public class Task2 {//Сочетания
-
+public class Task3 {
     public static void main(String[] args) {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
              BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))) {
 
+            System.out.println("Введите n и k:");
             String line = reader.readLine();
             String[] arr = line.split(" +");
             int n = Integer.parseInt(arr[0]);
             int k = Integer.parseInt(arr[1]);
 
-            if ((n < 1 || n > 7) || (k < 1 || k > 7)) {
-                System.err.println("Ошибка: Число должно находиться в диапазоне от 1 до 7.");
-                return;
+            if ((n < 1 || n > 4) || (k < 1 || k > 4)) {
+                throw new RuntimeException("Ошибка: Число должно находиться в диапазоне от 1 до 7.");
             }
 
-            long combination = factorial(n) / (factorial(k) * factorial(n - k));
+            long combination = factorial(n + k - 1) / (factorial(k) * factorial(n - 1));
 
             writer.write(String.valueOf(combination));
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
