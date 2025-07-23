bc91d8a932ff758689dcd327b0f2f70acd243a9b
Andrei <An.S-1@yandex.ru>
2025-07-23 03:45:45 +0300

    add 2

--- handbook/src/main/java/ru/practicum/Task6.java
+++ handbook/src/main/java/ru/practicum/Task6.java
@@ -1,15 +1,29 @@
 package ru.practicum;
 
 import java.io.*;
-
+//no
 public class Task6 {
     public static void main(String[] args) {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
              BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))) {
 
+            String line = reader.readLine();
+            String[] arr = line.split(" +");
+            int n = Integer.parseInt(arr[0]);
+            int m = Integer.parseInt(arr[1]);
+
+            if (isWinPosition(n, m)) {
+                writer.write("Win");
+            } else {
+                writer.write("Lose");
+            }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static boolean isWinPosition(int n, int m) {
+        return !(n % 3 == 0 && m % 3 == 0); //(Math.abs(n - m)) % 2 != 0 // Проверка условия победы/поражения
     }
 }
